95b0f651e90de54973028ff0e6ad71ae81263518
Aecio Silva <aeciofcs@gmail.com>
2021-07-12 00:44:28 -0300

    D02 - Test Integração - Test Auth User Controller

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -10,7 +10,7 @@ let connection: Connection;
 
 describe("Authenticate User", () => {
   beforeAll( async () => {
-    connection = await createConnection();
+    connection = await createConnection;
     await connection.runMigrations();
 
     const password = await hash("admin123", 8);
@@ -34,23 +34,30 @@ describe("Authenticate User", () => {
       password: "admin123"
     });
 
-    console.log(responseAuth.body)
     expect(responseAuth.status).toBe(200);
     expect(responseAuth.body).toHaveProperty("user");
     expect(responseAuth.body).toHaveProperty("token");
 
   });
 
-  /*
-  it("Should be no able to create a new user with name existing", async () => {
-    const response = await request(app).post("/api/v1/users").send({
-      name: "Aecio",
-      email: "aeciofcs@gmail.com",
+
+  it("Should be no able to authenticate an user with email not exists", async () => {
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "aeciofcs@finapi.com",
       password: "aecio123"
     });
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(401);
   });
-  */
+
+  it("Should be no able to authenticate an user with password invalid", async () => {
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "admin@finapi.com.br",
+      password: "aecio123"
+    });
+
+    expect(response.status).toBe(401);
+  });
+
 
 });
